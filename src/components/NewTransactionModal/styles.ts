84968c6e38e81ee1923a98b9b45c21b52a286832
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-01-22 14:47:46 -0300

    Visual structure - Creating accessible radio buttons

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -68,7 +69,7 @@ export const CloseButton = styled(Dialog.Close)`
   border-radius: 6px;
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -79,7 +80,9 @@ interface TransactionTypeButtonProps {
   $variant: "income" | "outcome";
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(
+  RadioGroup.Item
+)<TransactionTypeButtonProps>`
   background: ${(props) => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
@@ -96,5 +99,26 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
       props.$variant === "income"
         ? props.theme["green-300"]
         : props.theme["red-300"]};
+  }
+
+  &[data-state="unchecked"] {
+    transition: background-color 200ms;
+
+    &:hover {
+      background: ${(props) => props.theme["gray-600"]};
+    }
+  }
+
+  &[data-state="checked"] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.$variant === "income"
+        ? props.theme["green-500"]
+        : props.theme["red-500"]};
+    transition: background-color 200ms;
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
   }
 `;
