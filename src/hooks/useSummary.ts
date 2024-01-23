445960befb19a7f3dd6e5cef2d94b815613940dc
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-01-22 21:26:18 -0300

    Performance - Applying useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { TransactionsContext } from '../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
 
@@ -7,23 +8,27 @@ export function useSummary() {
     (context) => context.transactions,
   )
 
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price
-        accumulator.total += transaction.price
-      } else {
-        accumulator.outcome += transaction.price
-        accumulator.total -= transaction.price
-      }
+  const summary = useMemo(
+    () =>
+      transactions.reduce(
+        (accumulator, transaction) => {
+          if (transaction.type === 'income') {
+            accumulator.income += transaction.price
+            accumulator.total += transaction.price
+          } else {
+            accumulator.outcome += transaction.price
+            accumulator.total -= transaction.price
+          }
 
-      return accumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
+          return accumulator
+        },
+        {
+          income: 0,
+          outcome: 0,
+          total: 0,
+        },
+      ),
+    [transactions],
   )
 
   return summary
